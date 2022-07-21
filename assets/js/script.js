8d9c918ba036687a0860c44d1410e9253753dfc1
vagabond <athomec@pie.com.tw>
2022-07-22 00:14:17 +0800

    測試調整

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,65 +3,6 @@ $(function () {//JS開頭
 	var WINDOW = $(window).width();//視窗寬度
 	var WINDOWH = $(window).height();//視窗高度
 
-	//---------------------登入頁設定----------------------
-	$(".js-toggle-menu").find("input").click(function () {
-		$(this).parents(".js-toggle-menu").find(".form-check").removeClass('active');
-		$(this).parent(".form-check").addClass("active");
-	})
-
-	//---------------------側邊選單設定------------------------
-	$(".js-info-opener").click(function () {
-		$(".js-side-info").removeClass('open');
-		let content = $(this).attr("data-target");
-		$(".js-handler").addClass('open');
-		$(".js-map-content").addClass('open');
-		$(content).addClass('open');
-	})
-	$(".js-info-closer").click(function () {
-		$(".js-handler").removeClass('open');
-		$(".js-map-content").removeClass('open');
-		$(".js-side-info").removeClass('open');
-	})
-	$(".js-side-menu-toggler").click(function () {
-		$('.js-side-menu').toggleClass("close");
-		$(".js-side-content").toggleClass("close");
-	})
-	//---------------------視窗拖曳設定------------------------
-	var handler = document.querySelector('.js-handler');
-	var wrapper = handler.closest('.js-side-content');
-	var boxA = wrapper.querySelector('.js-map-content');
-	var boxB = wrapper.querySelector('.js-side-info');
-	var isHandlerDragging = false;
-	var isHandlertoching = false;
-
-	document.addEventListener('mousedown', function (e) {
-		// If mousedown event is fired from .handler, toggle flag to true
-		if (e.target === handler) {
-			isHandlerDragging = true;
-		}
-	});
-
-	document.addEventListener('mousemove', function (e) {
-		// Don't do anything if dragging flag is false
-		if (!isHandlerDragging) {
-			return false;
-		}
-		// Get offset
-		var containerOffsetTop = wrapper.offsetTop;
-		var containerOffsetBottom = wrapper.offsetBottom;
-		// Get x-coordinate of pointer relative to container
-		var pointerRelativeXpos = e.clientY - containerOffsetTop;
-		var pointerRelativeXpos2 = e.clientY - e.offsetTop + e.offsetHeight;
-		var boxAminWidth = 30;
-		boxA.style.height = (Math.max(boxAminWidth, pointerRelativeXpos - 2)) + 'px';
-		//boxA.style.flexGrow = 0;
-		boxB.style.height = (Math.max(boxAminWidth, pointerRelativeXpos2 - 8)) + 'px';
-		//boxB.style.flexGrow = 0;
-	});
-	document.addEventListener('mouseup', function (e) {
-		// Turn off dragging flag when user mouse is up
-		isHandlerDragging = false;
-	});
 
 	//---------------------頁籤設定------------------------
 	$('.js-toggle-menu').find(".js-toggle-button").click(function () {
@@ -103,11 +44,10 @@ $(function () {//JS開頭
 		var wh = $(window).height();//視窗高度
 	});
 
-	//主選單設定
-	$(".navbar-nav").find('a').click(function () {
+	//跳閱書籤設定
+	$(".js-link-bookmark").click(function () {
 		var section = $(this).attr("href");
-		$("html,body").animate({ scrollTop: $(section).offset().top - 66 }, 300);
-		$(".navbar-collapse").removeClass("show");
+		$("html,body").animate({ scrollTop: $(section).offset().top - 150 }, 300);
 		return false;
 	});
 
